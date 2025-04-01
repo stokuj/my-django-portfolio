1472018177d707ea94e5f751387e604c2c72dc38
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2025-04-01 18:21:23 +0200

    Uppere ase tag
    Conver all tags to be in upper case

--- personal_portfolio/main/models.py
+++ personal_portfolio/main/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()  # Zamiana na wielkie litery przed zapisem
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.name
-
 class Project(models.Model):
     title = models.CharField(max_length=200)
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True, default='thumbnails/default_image.png')
