c314f55216a44868235873bdec5be60bb13b8975
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2025-04-24 22:11:30 +0200

    Change to tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./personal_portfolio/main/templates/**/*.html",
+    "./personal_portfolio/main/static/src/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
