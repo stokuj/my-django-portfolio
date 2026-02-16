163c5c52aebccef872a10a0978384e6d8ce52a16
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2026-02-16 10:56:27 +0100

    Remove unused dependencies #32
    Remove unused dependencies #32

--- django/main/admin.py
+++ django/main/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
 from .models import Project, Tag, PageView
-from ckeditor.widgets import CKEditorWidget
-from ckeditor.fields import RichTextField
-
-class ProjectAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        RichTextField: {'widget': CKEditorWidget},
-    }
-
-admin.site.register(Project, ProjectAdmin)
-#admin.site.register(Project)
+admin.site.register(Project)
 admin.site.register(Tag)
 admin.site.register(PageView)
