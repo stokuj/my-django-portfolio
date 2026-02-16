56b6cdd6da899462855a789e00e2fc160b4e5679
Krystian Stasica <51967777+stokuj@users.noreply.github.com>
2026-02-16 13:18:58 +0100

    Update tests.py
    Add visitor counter middleware tests for session and route behavior

--- django/main/tests.py
+++ django/main/tests.py
@@ -186,3 +186,27 @@ class URLsTest(TestCase):
     def test_project_detail_url(self):
         response = self.client.get(f'/projects/{self.project.id}/')
         self.assertEqual(response.status_code, 200)
+
+
+class VisitorCountMiddlewareTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('home')
+        self.about_url = reverse('about')
+
+    def test_first_home_visit_increments_counter(self):
+        self.client.get(self.home_url)
+        self.assertEqual(PageView.get_instance().count, 1)
+
+    def test_second_home_visit_in_same_session_does_not_increment(self):
+        self.client.get(self.home_url)
+        self.client.get(self.home_url)
+        self.assertEqual(PageView.get_instance().count, 1)
+
+    def test_home_alias_path_is_counted(self):
+        self.client.get('/home/')
+        self.assertEqual(PageView.get_instance().count, 1)
+
+    def test_non_home_path_does_not_increment_counter(self):
+        self.client.get(self.about_url)
+        self.assertEqual(PageView.get_instance().count, 0)
